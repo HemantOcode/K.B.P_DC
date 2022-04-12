333dbb2ae3b007cc839769fe473030f95f0359ed
Hemant pal <palh8684@gmail.com>
2022-04-12 18:25:02 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,68 @@ import 'package:flutter_application_1/about/about.dart';
 import 'package:flutter_application_1/home.dart';
 import 'package:flutter_application_1/Auth/register.dart';
 import 'Auth/login.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
-void main() {
+const AndroidNotificationChannel channel = AndroidNotificationChannel(
+  "",
+  "",
+  description: "",
+  importance: Importance.high,
+  playSound: true,
+);
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+Future<void> _firebaseMessageBackgroundhandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+}
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessageBackgroundhandler);
+  await Firebase.initializeApp();
+
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+      alert: true, badge: true, sound: true);
   runApp(KBP());
 }
 
-class KBP extends StatelessWidget {
+class KBP extends StatefulWidget {
+  @override
+  State<KBP> createState() => _KBPState();
+}
+
+class _KBPState extends State<KBP> {
+  @override
+  void initState() {
+    super.initState();
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      RemoteNotification? notification = message.notification;
+      AndroidNotification? android = message.notification?.android;
+      if (notification != null && android != null) {
+        flutterLocalNotificationsPlugin.show(
+            notification.hashCode,
+            notification.title,
+            notification.body,
+            NotificationDetails(
+              android: AndroidNotificationDetails(
+                channel.id,
+                channel.name,
+                color: Colors.blue,
+                playSound: true,
+                icon: '@mipmap/ic_launcher',
+              ),
+            ));
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return (MaterialApp(
