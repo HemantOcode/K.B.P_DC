2cae5248d46e88a764f6765d157623e1ee41a008
Abhinav Yadav <mr.kumarabhinayadav@gmail.com>
2022-05-05 22:41:47 +0530

    inital commit

--- lib/auth/register.dart
+++ lib/auth/register.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_application_1/auth/providers/auth_provider.dart';
 import 'package:provider/provider.dart';
 import './utilites/constant.dart';
 
